379c4498107527861faf7a0d7fdb9e2d80835ace
simonkcoding <ykwok3@uncc.edu>
2018-11-16 18:45:57 -0500

    fuk

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,14 +1,32 @@
 const router = require('express').Router();
 const articlesController = require('../../controllers/articlesController')
 
+// // Matches with "/api/articles"
+// router.route("/")
+//     .get(articlesController.findAll)
+//     .post(articlesController.create);
+
 // Matches with "/api/articles"
-router.route("/")
-    .get(articlesController.findAll)
-    .post(articlesController.create);
+router.get("/", (req, res) => {
+    articlesController.findAll(req, res)
+})
+
+router.post("/", (req, res) => {
+    articlesController.create(req, res)
+})
 
 // Matches with "/api/articles/id"
 router.route("/:id")
     .get(articlesController.findById)
     .delete(articlesController.remove);
+
+// Matches with "/api/articles/id"
+router.get("/:id",(req,res)=>{
+    articlesController.findById(req, res)
+})
+
+router.delete("/:id",(req,res)=>{
+    articlesController.remove(req,res)
+})
 
 module.exports = router;
