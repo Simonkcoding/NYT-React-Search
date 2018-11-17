1c5b7269500c5582f0f06b2147577e86358af684
simonkcoding <ykwok3@uncc.edu>
2018-11-16 19:21:58 -0500

    fuk

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const apiRoutes = require('./api');
 //API Routes
 router.use("/api",apiRoutes);
 
-//If no APi routes are hit,send to react app
-router.use(function(req,res){
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})
+// //If no APi routes are hit,send to react app
+// router.use(function(req,res){
+//     res.sendFile(path.join(__dirname,'../client/build/index.html'))
+// })
 
 module.exports = router;
