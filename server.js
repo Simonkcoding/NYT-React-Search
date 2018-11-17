328af17ff91cb7a3c180b2bacd98397aca981956
simonkcoding <ykwok3@uncc.edu>
2018-11-16 19:03:06 -0500

    123

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(routes);
 
 //Serve up static assets (heroku)
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
+    app.use(express.static("client/build"))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 //Connect to database
